621f5b594c8629fc33ac45c5ecd97b75106c3737
Lorena Cabrera <marialorenacg@gmail.com>
2025-10-12 16:53:43 +0100

    correccion de ruta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,17 @@ import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideHttpClient() 
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
