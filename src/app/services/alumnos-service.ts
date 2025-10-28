eb9191068c2dd268d1a92e5358b5aa9c39ca98cc
Lorena Cabrera <marialorenacg@gmail.com>
2025-10-28 20:41:11 +0000

    cambio puerto endpoint al 3000 que es el puerto en el backend

--- src/app/services/alumnos-service.ts
+++ src/app/services/alumnos-service.ts
@@ -4,9 +4,9 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class AlumnosService {
+export class AlumnosService{
 
-  endpoint: string = "http://localhost:8081/api/alumnos";
+  endpoint: string = "http://localhost:3000/api/alumnos";
 
   constructor(private httpClient: HttpClient){}
 
