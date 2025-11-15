6591a0e2a1b76a81442a1df9615c8cb323c976d1
Lorena Cabrera <marialorenacg@gmail.com>
2025-11-15 20:02:17 +0000

    creacion de la pagina fotos

---
+++ src/app/fotos/fotos.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fotos',
+  templateUrl: './fotos.page.html',
+  styleUrls: ['./fotos.page.scss'],
+  standalone:false,
+})
+export class FotosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
